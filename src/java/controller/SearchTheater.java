f731f37c31002ed9f0ed85e82974b81aa749c6bc
Vux <vutahe180868@fpt.edu.vn>
2024-08-01 22:33:12 +0700

    Fix searchTheater

--- src/java/controller/SearchTheater.java
+++ src/java/controller/SearchTheater.java
@@ -26,32 +26,7 @@ public class SearchTheater extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String searchQuery = request.getParameter("query");
-
-        TheaterDAO theaterDAO = new TheaterDAO();
-        AccountDAO accountDAO = new AccountDAO();
-
-        Vector<Theater> theaters;
-        if (searchQuery == null || searchQuery.isEmpty()) {
-            theaters = theaterDAO.getAllTheaters();
-        } else {
-            theaters = theaterDAO.searchTheatersByManagerName(searchQuery);
-        }
-
-        Vector<Account> managers = new Vector<>();
-        Vector<Integer> staffCounts = new Vector<>();
-
-        for (Theater theater : theaters) {
-            Account manager = accountDAO.getManagerByTheaterID(theater.getId());
-            int staffCount = accountDAO.getStaffCountByTheaterID(theater.getId());
-            managers.add(manager);
-            staffCounts.add(staffCount);
-        }
-
-        request.setAttribute("theaters", theaters);
-        request.setAttribute("managers", managers);
-        request.setAttribute("staffCounts", staffCounts);
-
-
+        request.setAttribute("query", searchQuery);
         request.getRequestDispatcher("views/dashboard/managetheater.jsp").forward(request, response);
     }
 }
